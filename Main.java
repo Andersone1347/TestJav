d6d3a6c206df60e1c12371a15f25e9ee5979a48f
Andersone1347 <panov.deadman@yandex.ru>
2023-12-13 00:13:15 +0300

    save

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dsaпащжлдрпа cda");
+        System.out.println("dsaпащжлдрпа c      da dad         da");
     }
 }
